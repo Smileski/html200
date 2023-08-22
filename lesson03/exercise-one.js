30c369253716a6cba3d742e345fac58163efd38f
Ljubica S <117959937+Smileski@users.noreply.github.com>
2023-08-22 00:20:21 -0700

    Update exercise-one.js

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,8 @@
+let typeDrinks = ['water', 'soda', 'beer', 'wine', 'milk', 'yogurt'];
+console.log(typeDrinks);
+typeDrinks.push('juice');
+console.log(typeDrinks);
+typeDrinks.splice(2,1);
+console.log(typeDrinks);
+stringOftypeDrinks = typeDrinks.join(', ');
+console.log(stringOftypeDrinks);
