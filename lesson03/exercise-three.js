0c4ec3e63263a05ef53300f5d294bee0fc92dabd
Ljubica S <117959937+Smileski@users.noreply.github.com>
2023-08-24 06:34:35 -0700

    Update exercise-three.js

--- lesson03/exercise-three.js
+++ lesson03/exercise-three.js
@@ -3,3 +3,8 @@ animals.forEach(myFunction);
 function myFunction(item, index) {
   console.log(index+1 + ", " + item);
 }
+
+const animals = ["dogs", "cats", "cows", "horses", "mise"];
+for (i=0; i<animals.length; i++) {
+  console.log((i+1) + ", " + animals[i])
+}
